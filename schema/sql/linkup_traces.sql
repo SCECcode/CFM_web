71b7ceaa28b04660724f4fa695a9a3ecd61ee23c
Mei-Hui Su <mei@MeiPro.local>
2019-08-25 20:30:24 -0700

    change order

--- schema/sql/linkup_traces.sql
+++ schema/sql/linkup_traces.sql
@@ -1,9 +1,12 @@
 CREATE TEMP TABLE tmp_x AS
      SELECT name, concat(name,'-trace'), gid from OBJECT_tb;
 
+CREATE TEMP TABLE tmp_y AS
+     SELECT layer, gid from TRACE_tb_gid;
+
 UPDATE OBJECT_tb 
     SET TRACE_tb_gid = array_append(TRACE_tb_gid, TRACE_tb.gid)
-    FROM tmp_x, TRACE_tb
+    FROM TRACE_tb, tmp_x
     WHERE TRACE_tb.layer = tmp_x.concat
     AND tmp_x.gid = OBJECT_tb.gid;
 
