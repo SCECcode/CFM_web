f59e70f1cfbbc07c07f600cfc7bc0af65c60ee2f
Mei-Hui Su <mei@MeiPro.local>
2020-04-29 00:07:29 -0700

    try another method an array of traces_tb_gid

--- schema/sql/linkup_traces.sql
+++ schema/sql/linkup_traces.sql
@@ -6,7 +6,7 @@ CREATE TEMP TABLE tmp_x AS
      SELECT name, concat(name,'-trace'), gid from OBJECT_tb;
 
 UPDATE OBJECT_tb
-    SET TRACE_tb_gid = TRACE_tb.gid
+    SET TRACE_tb_gid = TRACE_tb.gid, TRACES_tb_gid = array_cat(TRACES_tb_gid, ARRAY [ TRACE_tb.gid ])
     FROM tmp_x, TRACE_tb
     WHERE TRACE_tb.layer = tmp_x.concat
     AND tmp_x.gid = OBJECT_tb.gid;
--- schema/sql/setup_schema.sql
+++ schema/sql/setup_schema.sql
@@ -85,5 +85,6 @@ CREATE TABLE OBJECT_tb (
    USGS_ID VARCHAR(100),
 
    blind integer DEFAULT 0,
-   blinds integer []
+   blinds integer [],
+   TRACES_tb_gid integer []
 );
