f23802e834a49e647f09aa4c1a1f0d3ae8e02d66
Mei-Hui Su <mei@MeiPro.local>
2021-03-23 00:57:56 -0700

    new sql

--- schema/scripts/convert-shp.sh
+++ schema/scripts/convert-shp.sh
@@ -4,9 +4,10 @@
 ## shp2pgsql is part of postgres sql toolkit
 
 #CFM53_db
-shp2pgsql -a -I -s GCS_WGS_1984 /Users/mei/scec/CFM/CFM5.3_release_2021/obj/traces/shp/CFM5.3_traces.shp TRACE_tb > CFM53_preferred_traces_nonblind.sql
+shp2pgsql -I -s GCS_WGS_1984 /home/mei/CFM_web/schema/CFM5_release_2021/shp/CFM5.3_traces.shp nonblind_trace_tb > CFM53_preferred_traces_nonblind.sql
 
-shp2pgsql -a -I -s GCS_WGS_1984 /Users/mei/scec/CFM/CFM5.3_release_2021/obj/traces/shp/CFM5.3_blind.shp TRACE_tb > CFM53_preferred_traces_blind.sql
+shp2pgsql -I -s GCS_WGS_1984 /home/mei/CFM_web/schema/CFM5_release_2021/shp/CFM5.3_blind.shp blind_trace_tb > CFM53_preferred_traces_blind.sql
+
 
 #>>MeiPro:CFM5_preferred_traces mei$ ./convert-shp.sh
 #>>Shapefile type: ArcZ
