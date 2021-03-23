6bde99938072d9eda878c435a80ed7e76f34b377
Mei-Hui Su <mei@MeiPro.local>
2021-03-23 00:39:28 -0700

    sql does not need to create trace_tb

--- schema/scripts/convert-shp.sh
+++ schema/scripts/convert-shp.sh
@@ -4,7 +4,7 @@
 ## shp2pgsql is part of postgres sql toolkit
 
 #CFM53_db
-shp2pgsql -I -s GCS_WGS_1984 /Users/mei/scec/CFM/CFM5.3_release_2021/obj/traces/shp/CFM5.3_traces.shp TRACE_tb > CFM53_preferred_traces_nonblind.sql
+shp2pgsql -a -I -s GCS_WGS_1984 /Users/mei/scec/CFM/CFM5.3_release_2021/obj/traces/shp/CFM5.3_traces.shp TRACE_tb > CFM53_preferred_traces_nonblind.sql
 
 shp2pgsql -a -I -s GCS_WGS_1984 /Users/mei/scec/CFM/CFM5.3_release_2021/obj/traces/shp/CFM5.3_blind.shp TRACE_tb > CFM53_preferred_traces_blind.sql
 
