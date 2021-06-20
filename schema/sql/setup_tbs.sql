e3e427ee8200ae746a6702630e66360fbbbd8902
Mei-Hui Su <mei@MeiPro.local>
2021-06-19 21:57:28 -0700

    trying to split the eq_tb into multiple smaller files

--- schema/sql/setup_tbs.sql
+++ schema/sql/setup_tbs.sql
@@ -15,4 +15,6 @@ FROM '/home/postgres/CFM/schema/data/object_500m_tb.csv' DELIMITER ',' CSV HEADE
 COPY OBJECT_native_tb(name, url)
 FROM '/home/postgres/CFM/schema/data/object_native_tb.csv' DELIMITER ',' CSV HEADER;
 COPY EQ_tb(Dataset,EventTime,EventID,Lat,Lon,Depth,Mag,Easting,Northing)
-FROM '/home/postgres/CFM/schema/data/eq_tb.csv' DELIMITER ',' CSV HEADER;
+FROM '/home/postgres/CFM/schema/data/eq_Hauksson_1_tb.csv' DELIMITER ',' CSV HEADER;
+COPY EQ_tb(Dataset,EventTime,EventID,Lat,Lon,Depth,Mag,Easting,Northing)
+FROM '/home/postgres/CFM/schema/data/eq_Hauksson_2_tb.csv' DELIMITER ',' CSV HEADER;
