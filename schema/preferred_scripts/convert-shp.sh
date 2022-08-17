f0f0eb1e991380e154f9912be96ba1f991597b6c
Mei-Hui Su <mei@MeiPro.local>
2022-08-17 09:24:15 -0700

    wip

--- schema/preferred_scripts/convert-shp.sh
+++ schema/preferred_scripts/convert-shp.sh
@@ -3,10 +3,12 @@
 ## convert from shp file content to sql import script 
 ## shp2pgsql is part of postgres sql toolkit
 
-#CFM53_db
-shp2pgsql -I -s GCS_WGS_1984 /users/mei/scec/CFM_web/schema/CFM5_release_2021/shp/CFM5.3_traces.shp nonblind_trace_tb > CFM53_preferred_traces_nonblind.sql
+export CFMLOC="/users/mei/scec/CFM_web/schema/CFM5_release_2021/shp"
 
-shp2pgsql -I -s GCS_WGS_1984 /users/mei/scec/CFM_web/schema/CFM5_release_2021/shp/CFM5.3_blind.shp blind_trace_tb > CFM53_preferred_traces_blind.sql
+#CFM53_db
+shp2pgsql -I -s GCS_WGS_1984 ${CFMLOC}/CFM5.3_traces.shp nonblind_trace_tb > CFM53_preferred_traces_nonblind.sql
+
+shp2pgsql -I -s GCS_WGS_1984 ${CFMLOC}/CFM5.3_blind.shp blind_trace_tb > CFM53_preferred_traces_blind.sql
 
 
 #>>MeiPro:CFM5_preferred_traces mei$ ./convert-shp.sh
