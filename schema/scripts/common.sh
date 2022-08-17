754732c6edcd7b8eea1b935a60b6c5b5bb2c2ee0
meihuisu <mei@ip-172-31-31-151.us-west-2.compute.internal>
2022-08-17 22:08:26 +0000

    wip

---
+++ schema/scripts/common.sh
@@ -0,0 +1,25 @@
+#CFM5.3_Metadata.xlsx, CFM53_preferred_db
+#CFMPATH="/home/mei/CFM_web/schema/CFM5.3_latest/"
+#CFMLOC=${CFMPATH}"shp/CFM5.3"
+#CFMTYPE="CFM53_preferred"
+#EXCEL_NM="CFM5.3_Metadata"
+
+# CFM6.0_Alt_Metadata.xlsx, CFM6_alt_db
+#CFMPATH="/home/mei/CFM_web/schema/CFM6_alternatives/"
+#CFMLOC=${CFMPATH}"shp/CFM6.0_Alt"
+#CFMTYPE="CFM6_alt"
+#EXCEL_NM="CFM6.0_Alt_Metadata"
+
+#CFM6.0_Metadata.xlsx, CFM6_preferred_db
+#CFMPATH="/home/mei/CFM_web/schema/CFM6/"
+#CFMLOC=${CFMPATH}"shp/CFM6.0"
+#CFMTYPE="CFM6_preferred"
+#EXCEL_NM="CFM6.0_Metadata"
+
+#CFM6.0_Rup_Metadata.xlsx, CFM6_rup_db
+CFMPATH="/home/mei/CFM_web/schema/CFM6_ruptures/"
+CFMLOC=${CFMPATH}"shp/CFM6.0_Rup"
+CFMTYPE="CFM6_rup"
+EXCEL_NM="CFM6.0_Rup_Metadata"
+
+
