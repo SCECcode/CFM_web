1d52e6cfe6169ad4d4ecd8b404939cd5d7677e54
meihuisu <mei@ip-172-31-31-151.us-west-2.compute.internal>
2022-12-05 16:08:21 +0000

    add prelimary CFM6

--- schema/scripts/common.sh
+++ schema/scripts/common.sh
@@ -1,3 +1,17 @@
+#####  processing schema/data
+#####  cp target_data's  doc/target.csv here
+#####  unmark the data type at the bottom
+#####
+#####    ./extract-csv.sh
+#####        mv *.csv to  schema/sql/target
+#####    ./convert-shp.sh
+#####        mv *.sql to  schema/sql/target
+#####    ./create-csv.sh
+#####        mv *.csv to  schema/data/target
+#####    ./create-sql.sh
+#####        mv *.sql to  schema/sql/target
+#####
+
 
 PWD=`pwd`
 
@@ -10,13 +24,13 @@ PWD=`pwd`
 #AWSPATH="https://s3-us-west-2.amazonaws.com/files.scec.org/s3fs-public/projects/cfm/CFM5/CFM53_preferred"
 
 # CFM6.0_Alt_Metadata.xlsx, CFM6_alt_db
-#CFMPATH=${PWD}"/../CFM6_alternatives/"
-#CFMLOC=${CFMPATH}"shp/CFM6.0_traces_Alt"
-#CFMLOC_BLIND=${CFMPATH}"shp/CFM6.0_blind_Alt"
-#CFMTYPE="CFM6_alt"
-#EXCEL_NM="CFM6.0_Metadata_Alt"
-#DATATYPE="alternatives6"
-#AWSPATH="https://s3-us-west-2.amazonaws.com/files.scec.org/s3fs-public/projects/cfm/CFM6/CFM6_Alternatives"
+CFMPATH=${PWD}"/../CFM6_alternatives/"
+CFMLOC=${CFMPATH}"shp/CFM6.0_traces_Alt"
+CFMLOC_BLIND=${CFMPATH}"shp/CFM6.0_blind_Alt"
+CFMTYPE="CFM6_alt"
+EXCEL_NM="CFM6.0_Metadata_Alt"
+DATATYPE="alternatives6"
+AWSPATH="https://s3-us-west-2.amazonaws.com/files.scec.org/s3fs-public/projects/cfm/CFM6/CFM6_Alternatives"
 
 #CFM6.0_Metadata.xlsx, CFM6_preferred_db
 #CFMPATH=${PWD}"/../CFM6/"
@@ -29,10 +43,10 @@ PWD=`pwd`
 
 
 #CFM6.0_Rup_Metadata.xlsx, CFM6_rup_db
-CFMPATH=${PWD}"/../CFM6_ruptures/"
-CFMLOC=${CFMPATH}"shp/CFM6.0_traces_Rup"
-CFMLOC_BLIND=${CFMPATH}"shp/CFM6.0_blind_Rup"
-CFMTYPE="CFM6_rup"
-EXCEL_NM="CFM6.0_Metadata_Rup"
-DATATYPE="ruptures6"
-AWSPATH="https://s3-us-west-2.amazonaws.com/files.scec.org/s3fs-public/projects/cfm/CFM6/CFM6_Ruptures"
+#CFMPATH=${PWD}"/../CFM6_ruptures/"
+#CFMLOC=${CFMPATH}"shp/CFM6.0_traces_Rup"
+#CFMLOC_BLIND=${CFMPATH}"shp/CFM6.0_blind_Rup"
+#CFMTYPE="CFM6_rup"
+#EXCEL_NM="CFM6.0_Metadata_Rup"
+#DATATYPE="ruptures6"
+#AWSPATH="https://s3-us-west-2.amazonaws.com/files.scec.org/s3fs-public/projects/cfm/CFM6/CFM6_Ruptures"
