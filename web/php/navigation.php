675d8b3b1d2c2147ec081a6fbc078ed42af03aa5
meihuisu <mei@ip-172-31-31-151.us-west-2.compute.internal>
2024-08-14 00:09:39 +0000

    wip

--- web/php/navigation.php
+++ web/php/navigation.php
@@ -40,7 +40,10 @@ _END;
 <div id="banner-container" class="banner-container">
     <div class="container top">
         <nav class="navbar navbar-expand-lg navbar-dark  scec-header">
+            <a class="navbar-brand" href="explorer.php"><img class="scec-logo" src="img/sceclogo_transparent.png">
+<!--
             <a class="navbar-brand" href="$host_site_actual_path"><img class="scec-logo" src="img/sceclogo_transparent.png">
+-->
                 &nbsp;Community Fault Model Explorer</a>
                   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
