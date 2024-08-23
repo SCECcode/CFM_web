f1bae976c82b97f08ce7ba0e294434482ec6ae94
Edric Pauk <pauk@usc.edu>
2024-08-23 13:31:45 -0700

    refactor for loading multiple models without extra containers

--- web/js/cfm_view3d.js
+++ web/js/cfm_view3d.js
@@ -226,7 +226,8 @@ window.addEventListener("DOMContentLoaded", function () {
     if (origin != "http://localhost:8084" && origin != "http://moho.scec.org"
              && origin != "https://moho.scec.org" 
                && origin != "http://training.scec.org"
-        && origin != "https://www.scec.org"
+        && origin != "https://cfm.localhost"
+        && origin != "https://central.scec.org"
         && origin != "https://stress.scec.org") {
         window.console.log("SERVER <<< bad message origin:", origin);
         return;
