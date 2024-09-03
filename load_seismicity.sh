cd1deee0c29ffac615e7715564f8d1732e477a00
meihuisu <mei@ip-172-31-31-151.us-west-2.compute.internal>
2024-09-03 23:47:32 +0000

    fix typo

--- load_seismicity.sh
+++ load_seismicity.sh
@@ -2,22 +2,22 @@
 
 mkdir -p seismicity
 cd seismicity
-wget https://files.scec.org/s3fs-public/projects/cfm/seisimicty/cfm-v7.0-seisimicity-cache.tar.gz
-#tar xvf /home/mei/CFM_web_target/cfm-v7.0-seisimicity-cache.tar.gz
+wget https://files.scec.org/s3fs-public/projects/cfm/seismicty/cfm-v7.0-seismicity-cache.tar.gz
+#tar xvf /home/mei/CFM_web_target/cfm-v7.0-seismicity-cache.tar.gz
 cd ..
 mv seismicity web/data
 
 mkdir -p cfm_data
 cd cfm_data
-wget https://files.scec.org/s3fs-public/projects/cfm/seisimicty/cfm-v7.0-seisimicity-utm.tar.gz
-#tar xvf /home/mei/CFM_web_target/cfm-v7.0-seisimicity-utm.tar.gz
+wget https://files.scec.org/s3fs-public/projects/cfm/seismicty/cfm-v7.0-seismicity-utm.tar.gz
+#tar xvf /home/mei/CFM_web_target/cfm-v7.0-seismicity-utm.tar.gz
 cd ..
 mv cfm_data web
 
 mkdir -p schema_data
 cd schema_data
-wget https://files.scec.org/s3fs-public/projects/cfm/seisimicty/cfm-v7.0-seisimicity-data.tar.gz
-#tar xvf /home/mei/CFM_web_target/cfm-v7.0-seisimicity-data.tar.gz
+wget https://files.scec.org/s3fs-public/projects/cfm/seismicty/cfm-v7.0-seismicity-data.tar.gz
+#tar xvf /home/mei/CFM_web_target/cfm-v7.0-seismicity-data.tar.gz
 cd ..
 mv schema_data/* schema/data
 rm -rf schema_data
