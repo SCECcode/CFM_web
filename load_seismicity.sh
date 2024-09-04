2e73603bf7d2d87ff7704a85a5f66482932dbb37
meihuisu <mei@ip-172-31-31-151.us-west-2.compute.internal>
2024-09-04 07:14:30 +0000

    add load fix

--- load_seismicity.sh
+++ load_seismicity.sh
@@ -17,7 +17,6 @@ mv cfm_data web
 mkdir -p schema_data
 cd schema_data
 wget https://files.scec.org/s3fs-public/projects/cfm/seismicity/cfm-v7.0-seismicity-data.tar.gz
-tar cfm-v7.0-seismicity-data.tar.gz
+tar xvf cfm-v7.0-seismicity-data.tar.gz
 cd ..
 mv schema_data/* schema/data
-rm -rf schema_data
