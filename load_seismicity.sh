f502f563fad882541302f4b2afaeea07709f58a2
meihuisu <mei@ip-172-31-31-151.us-west-2.compute.internal>
2024-10-03 16:56:55 +0000

    push the wget for schema/data to Docker

--- load_seismicity.sh
+++ load_seismicity.sh
@@ -1,12 +1,5 @@
 #/bin/bash
 
-#mkdir -p seismicity
-#cd seismicity
-#wget https://files.scec.org/s3fs-public/projects/cfm/seismicity/cfm-v7.0-seismicity-cache.tar.gz
-#tar xvf cfm-v7.0-seismicity-cache.tar.gz
-#cd ..
-#mv seismicity web/data
-
 mkdir -p cfm_data
 cd cfm_data
 wget https://files.scec.org/s3fs-public/projects/cfm/seismicity/cfm-v7.0-seismicity-utm.tar.gz
@@ -14,9 +7,9 @@ tar xvf cfm-v7.0-seismicity-utm.tar.gz
 cd ..
 mv cfm_data web
 
-mkdir -p schema_data
-cd schema_data
-wget https://files.scec.org/s3fs-public/projects/cfm/seismicity/cfm-v7.0-seismicity-data.tar.gz
-tar xvf cfm-v7.0-seismicity-data.tar.gz
-cd ..
-mv schema_data/* schema/data
+#mkdir -p schema_data
+#cd schema_data
+#wget https://files.scec.org/s3fs-public/projects/cfm/seismicity/cfm-v7.0-seismicity-data.tar.gz
+#tar xvf cfm-v7.0-seismicity-data.tar.gz
+#cd ..
+#mv schema_data/* schema/data
