a7f0c16440c3e2c115cf14b428af592fcf86384b
meihuisu <mei@ip-172-31-31-151.us-west-2.compute.internal>
2026-07-11 15:16:31 +0000

    done

--- web/js/cfm_view3d.js
+++ web/js/cfm_view3d.js
@@ -600,7 +600,7 @@ function resetExpand3Dview() {
 function save3Dview() {
   document.getElementById("view3DIfram").contentDocument.getElementById("Downloadbtn").click();
   let cmd="cfm_3d.html?"+get_PARAMS();
-  window.console.log(cmd);
+  window.console.log("save3Dview:", cmd);
 }
 
 // pop up the notify model with a timeout
